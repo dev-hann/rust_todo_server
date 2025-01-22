4d697c6b251bfc7130ea4ba1bef66a5dedd57c2e
hann <developer.hann@gmail.com>
2025-01-23 07:07:52 +0900

    🎨 Feat: jwt & middleware

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,11 @@ mod models;
 mod service;
 use models::claims::AuthToken;
 
-#[get("/protected")]
-async fn protected_route(_auth: AuthToken) -> &'static str {
-    "This is a protected route!"
-}
 
 #[launch]
 fn rocket() -> _ {
     rocket::build().manage(models::app_state::AppState::new())
-        .mount("/api/todo", routes![protected_route])
+        .attach(AuthToken)
         .mount("/api/todo",routers::todo::routes())
         .mount("/api/auth",routers::auth::routes())
-        .attach(AuthToken)
 }
--- src/models/claims.rs
+++ src/models/claims.rs
@@ -1,12 +1,10 @@
 use serde::{Deserialize, Serialize};
-use jsonwebtoken::{encode, Header};
-use jsonwebtoken::{DecodingKey, EncodingKey};
-use rocket::http::Status;
-use rocket::request::{self, FromRequest, Request};
-use jsonwebtoken::{decode, Validation, Algorithm};
-use std::time::{SystemTime, UNIX_EPOCH};
+use jsonwebtoken::{ decode, encode, DecodingKey, EncodingKey, Header, Validation};
+use rocket::request::Request;
 use rocket::fairing::{Fairing, Info, Kind};
-
+use rocket::Data;
+use rocket::response::Response;
+use rocket::http::Status;
 #[derive(Debug,Clone,Serialize,Deserialize)]
 pub struct Claims {
     pub user_id: u32,
@@ -31,41 +29,41 @@ impl Claims {
 }
 
 #[derive(Debug)]
-pub struct AuthToken(Claims);
-
-#[rocket::async_trait]
-impl<'r> FromRequest<'r> for AuthToken {
-    type Error = Status;
-
-    async fn from_request(request: &'r Request<'_>) -> request::Outcome<Self, Self::Error> {
-        // Authorization 헤더에서 토큰 추출
-        if let Some(auth_header) = request.headers().get_one("Authorization") {
-            if let Some(token) = auth_header.strip_prefix("Bearer ") {
-                let validation = Validation::new(Algorithm::HS256);
-                // 토큰 검증
-                match decode::<Claims>(token, &DecodingKey::from_secret(SECRETKEY.as_ref()), &validation) {
-                    Ok(token_data) => {
-                        return request::Outcome::Success(AuthToken(token_data.claims));
-                    },
-                    Err(e) => return request::Outcome::Error((Status::Unauthorized, Status::UnavailableForLegalReasons)),
-                }
-            }
-        }
-        request::Outcome::Error((Status::Unauthorized, Status::UnavailableForLegalReasons))
-    }
-}
+pub struct AuthToken;
 
 #[rocket::async_trait]
 impl Fairing for AuthToken {
     fn info(&self) -> Info {
         Info {
             name: "AuthToken",
-            kind: Kind::Request,
+            kind: Kind::Request | Kind::Response,
         }
     }
 
-    fn on_request(&self, request: &Request<'_>, response: &mut Response<'_>) {
-        println!("Request: {:?}", request);
-        println!("Response: {:?}", response);
+    async fn on_request(&self, req: &mut Request<'_>, _: &mut Data<'_>) {
+        let path = req.uri().path();
+        if path.starts_with("/api/auth") {
+            println!("Skipping authentication for /api/auth path");
+            return;
+        }
+        let header = req.headers().get_one("Authorization");
+        match header {
+            Some(auth_header) => {
+                if auth_header.starts_with("Bearer ") {
+                    let token = auth_header.split_whitespace().nth(1).expect("Bearer token expected");
+                    let token_data = decode::<Claims>(token, &DecodingKey::from_secret(SECRETKEY.as_ref()), &Validation::default());
+                    println!("Token data: {:?}", token_data);
+                }
+            }
+            None => {
+                req.local_cache(|| Some(rocket::http::Status::Unauthorized));
+            }
+        }
+
+    }
+
+    async fn on_response<'r>(&self, req: &'r Request<'_>, res: &mut Response<'r>) {
+        println!("response {:?}", res);
+        println!("request {:?}", req);
     }
 }
